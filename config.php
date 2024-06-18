b55aaefbb2c640b52d29b89746180ed0fbb1e68c
Renan Carvalho Manera <126332577+renancmanera@users.noreply.github.com>
2024-06-18 18:38:12 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$dbHost = 'Localhost';
+$dbUsername = 'root';
+$dbPassword = 'renan2004';
+$dbName = 'formulario';
+
+$conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName)
+
+if($conexao->connect_errno)
+{
+    echo "Erro";
+}
+else
+{
+    echo "Conexão efetuada com sucesso!"
+}
+
+?>
---
+++ header.js
@@ -0,0 +1,71 @@
+
+        function verificarHeader() {
+            const numeroDigitado = document.getElementById('numeroInput').value;
+            if (numeroDigitado === '1') {
+                // Redireciona para outra página (substitua a URL desejada)
+                window.location.href = "adicionar.html";
+            } else if (numeroDigitado === '2') {
+                window.location.href = "editar.html";
+            } else if (numeroDigitado === '3') {
+                window.location.href = "excluir.html";
+            } else if (numeroDigitado === '4') {
+                window.location.href = "pesquisar.html";
+            } else {
+                alert('Digite um número válido ou a palavra "pesquisar".');
+            }
+        }
+
+        function verificarAdicionar() {
+            const numeroDigitado = document.getElementById('numeroInput').value;
+            if (numeroDigitado === '1') {
+                // Redireciona para outra página (substitua a URL desejada)
+                window.location.href = "header.html"; 
+            }
+            else {
+                alert('Digite um número válido.'); //PRECISO ARMAZENAR
+
+        }
+    }      
+
+    function verificarEditar() {
+        const numeroDigitado = document.getElementById('numeroInput').value;
+        if (numeroDigitado === '1') {
+            // Redireciona para outra página (substitua a URL desejada)
+            window.location.href = "header.html"; 
+        }
+        else {
+            alert('Digite um número válido.'); //PRECISO ARMAZENAR
+
+    }
+}   
+        
+        function verificarExcluir() {
+        const numeroDigitado = document.getElementById('numeroInput').value;
+        if (numeroDigitado === '1') {
+            // Redireciona para outra página (substitua a URL desejada)
+            window.location.href = "header.html"; 
+        }
+        else {
+            alert('Digite um número válido.'); //PRECISO ARMAZENAR
+
+    }
+}
+
+function verificarPesquisar() {
+    const numeroDigitado = document.getElementById('numeroInput').value;
+    if (numeroDigitado === '1') {
+        // Redireciona para outra página (substitua a URL desejada)
+        window.location.href = "header.html"; 
+    }
+    else {
+        alert('Digite um número válido.'); //PRECISO ARMAZENAR
+
+}
+}
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        document.getElementById('enviarButton').click();
+    }
+});
+
